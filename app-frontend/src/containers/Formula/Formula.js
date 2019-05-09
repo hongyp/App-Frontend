4ac5f691a29f41afc553bd17d30c07826cb62e81
洪逸鹏 <PP@hongyipengde-MacBook-Pro.local>
2019-05-08 23:51:09 -0400

    nav bar

---
+++ app-frontend/src/containers/Formula/Formula.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react' 
+
+class Formula extends Component {
+
+    render () {
+        return (
+            <div className="formula-page">
+                Welcome to Formular page!
+            </div>
+        )
+    }
+
+}
+
+export default Formula;
---
+++ app-frontend/src/containers/Resource/Resource.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Resource extends Component {
+
+    render () {
+        return (
+            <div className="resource-page">
+                Welcome to Resource Page!!!
+            </div>
+        );
+    }
+}
+
+export default Resource;
