1fdf57922214ae3e1abdade8bf17e34106ab13bd
洪逸鹏 <PP@hongyipengde-MacBook-Pro.local>
2019-05-14 11:10:33 -0400

    JWT for login

--- ResourceApp/src/main/java/com/itlize/ResourceApp/Controller/UsersController.java
+++ ResourceApp/src/main/java/com/itlize/ResourceApp/Controller/UsersController.java
@@ -1,13 +1,20 @@
 package com.itlize.ResourceApp.Controller;
 
+import static org.springframework.http.ResponseEntity.ok;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itlize.ResourceApp.domain.Users;
+import com.itlize.ResourceApp.security.JwtTokenProvider;
 import com.itlize.ResourceApp.service.UsersService;
 
 @RestController
@@ -16,13 +23,21 @@ public class UsersController {
 
 	@Autowired
 	UsersService usersService;
+	@Autowired
+	JwtTokenProvider jwtTokenProvider;
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public Users loginValidation(@RequestBody Users user) throws Exception {
+	public ResponseEntity<?> loginValidation(@RequestBody Users user) throws Exception {
 		String email = user.getEmail();
 		String password = user.getPassword();
-		return usersService.loginValidation(email, password);
-//		return usersService.getUserByUsername(user.getEmail());
+		usersService.loginValidation(email, password);
+		String token = jwtTokenProvider.createTokenWithClaim(email);
+		System.err.println(token);
+//		jwtTokenProvider.verifyToken(token);
+		Map<Object, Object> model = new HashMap<>();
+        model.put("username", email);
+        model.put("token", token);
+        return ok(model);
 	}
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
