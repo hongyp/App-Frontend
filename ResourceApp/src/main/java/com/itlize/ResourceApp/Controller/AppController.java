3743ced0db6791e3e5d517f902ccf9ae668a2aa5
洪逸鹏 <PP@hongyipengde-MacBook-Pro.local>
2019-05-15 10:28:23 -0400

    Get table

--- ResourceApp/src/main/java/com/itlize/ResourceApp/Controller/AppController.java
+++ ResourceApp/src/main/java/com/itlize/ResourceApp/Controller/AppController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.itlize.ResourceApp.service.AppService;
 
 @RestController
 @EnableAutoConfiguration
+@CrossOrigin(origins="http://localhost:3000")
 public class AppController  {
 
 	@Autowired
